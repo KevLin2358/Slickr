a73ea0946e534f61dfdb5008c52b6b29e3a38a16
Kevin Lin <klin2358@gmail.com>
2021-09-04 21:11:35 -0400

    login, create, logout routes working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+  protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
   private
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
   end
 
   def login(user)
-    session[:session_token] = user.reset_session_token!
     @current_user = user
+    session[:session_token] = user.reset_session_token!
   end
 
   def logout
