80b3d3b30f05c13a5a0300b9fa5d1e856158ac9f
Kevin Lin <klin2358@gmail.com>
2021-09-08 23:41:26 -0400

    heroku test

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => (
   <footer className="footer-nav">
     <div>About Me</div>
     <div>Portfolio</div>
-    <img className ="github" src='app/assets/images/github.png' />
-    <img className ="linkedin" src='app/assets/images/linkedin.png' />
+    <img className ="github" src={require('../../../app/assets/images/github.png')} />
+    <img className ="linkedin" src='../../../app/assets/images/linkedin.png' />
   </footer>
 )
 
