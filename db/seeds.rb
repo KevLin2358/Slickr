1569c2827121d80b958e66eb2778ca9246d1ea81
Kevin Lin <klin2358@gmail.com>
2021-09-09 18:09:57 -0400

    seed demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+demo1 = User.create!({email: 'abcdefg', first_name: 'abcdefg', last_name: 'abcdefg', username: 'abcdefg', password: 'abcdefg'})
